2ce8b51fda3cbc5343a5b103a158a357a93baba2
floppochka <amogusputus@gmail.com>
2024-10-28 19:33:13 +0300

    Add files via upload

--- Kugushev/5zan.py
+++ Kugushev/5zan.py
@@ -13,7 +13,30 @@ for i in range(10): #создает счет от 0 до 4 в переменно
     if da[i]%3 ==0:
         da[i]=0
     print(da[i])
- '''
+ 
 #создаем случ массив 3 на 3
 arrr=np.random.randint(1,100,size=(3,3))
 print(arrr)
+
+
+a = np.array([34, 56, 99, 43, 98, 2])
+for i in range(5):
+    if a[i]>x:
+        x=a[i]
+
+print(x)
+
+
+
+a=50
+grrib=3
+da=a//grrib
+print(a-da*grrib)
+'''
+
+a=np.array([3,4,2,1,5,6,0])
+sum=0
+for i in range(len(a)):
+    sum=a[0] + i
+    print(sum)
+    
